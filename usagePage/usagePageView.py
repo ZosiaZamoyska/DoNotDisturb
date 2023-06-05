33422e805aa84f32e25ea6abb3d012ea6b0cb2ee
zhilin <zhilin_yap@yahoo.com>
2023-06-06 03:15:33 +0900

    did weekinreview

--- usagePage/usagePageView.py
+++ usagePage/usagePageView.py
@@ -4,7 +4,8 @@ from usagePage.emotion import to_emoji
 import pandas as pd
 import plotly.express as px
 import matplotlib
-
+import statistics
+import datetime
 
 class UsagePageView:
     def get_model(self):
@@ -77,69 +78,364 @@ class UsagePageView:
         return div
 
     def _build_review_widget(self):
-        day = (
-            ["monday"] * 7
-            + ["tuesday"] * 7
-            + ["wednesday"] * 7
-            + ["thursday"] * 7
-            + ["friday"] * 7
-            + ["saturday"] * 7
-            + ["sunday"] * 7
-        )
-        time = [
-            [6, 2, 2, 3, 4, 3, 4],
-            [2, 6, 2, 3, 4, 5, 2],
-            [6, 1, 2, 4, 4, 2, 5],
-            [8, 2, 3, 2, 1, 4, 4],
-            [7, 3, 1, 2, 4, 5, 2],
-            [9, 2, 2, 4, 3, 4],
-            [6, 2, 2, 3, 4, 3, 4],
-        ]
-        # colors = ['r', 'g', 'b', 'g', 'b', 'g', 'r']
-        colors = [
-            [0, 2, 3, 1, 3, 1, 0],
-            [1, 0, 3, 0, 3, 2, 1],
-            [0, 2, 3, 1, 3, 2, 1],
-            [0, 3, 2, 1, 2, 3, 1],
-            [0, 2, 1, 2, 3, 2, 0],
-            [0, 2, 3, 2, 3, 1],
-            [0, 2, 1, 2, 3, 2, 1],
-        ]
-        df = pd.DataFrame(
-            list(zip(day, time, colors)), columns=["day", "time", "colors"]
-        )
-        # fig = px.bar(df, x=time, y=day, color=colors, width=1000, height=300)
-        z = [[], [], [], [], [], [], []]
-        color_map = matplotlib.cm.get_cmap("Paired_r", 10)
+        schedule = pd.read_csv("schedule.csv")
+        schedule['Start'] = pd.to_datetime(schedule[['From Hour', 'From Min']].astype(str).apply(':'.join, axis=1) + ':00', format='%H:%M:%S').dt.time
+        schedule['End'] = pd.to_datetime(schedule[['To Hour', 'To Min']].astype(str).apply(':'.join, axis=1) + ':00', format='%H:%M:%S').dt.time
+        # for the first day 
+        phoneUsage_day1 = pd.read_csv("review_entries_day1.csv")
+        phoneUsage_day1['startTime'] = pd.to_datetime(phoneUsage_day1['startTime']).dt.time
+        phoneUsage_day1['endTime'] = pd.to_datetime(phoneUsage_day1['endTime']).dt.time
+        phoneUsage_day1['timestamp'] = pd.to_datetime(phoneUsage_day1['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day1['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day1 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day1.iterrows():
+            for index_p, row_p in phoneUsage_day1.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day1["Ratings"] = final_rating
+
+        # for the second day 
+        phoneUsage_day2 = pd.read_csv("review_entries_day2.csv")
+        phoneUsage_day2['startTime'] = pd.to_datetime(phoneUsage_day2['startTime']).dt.time
+        phoneUsage_day2['endTime'] = pd.to_datetime(phoneUsage_day2['endTime']).dt.time
+        phoneUsage_day2['timestamp'] = pd.to_datetime(phoneUsage_day2['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day2['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day2 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day2.iterrows():
+            for index_p, row_p in phoneUsage_day2.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day2["Ratings"] = final_rating
+
+        # for day 3
+        phoneUsage_day3 = pd.read_csv("review_entries_day3.csv")
+        phoneUsage_day3['startTime'] = pd.to_datetime(phoneUsage_day3['startTime']).dt.time
+        phoneUsage_day3['endTime'] = pd.to_datetime(phoneUsage_day3['endTime']).dt.time
+        phoneUsage_day3['timestamp'] = pd.to_datetime(phoneUsage_day3['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day3['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day3 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day3.iterrows():
+            for index_p, row_p in phoneUsage_day3.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day3["Ratings"] = final_rating
+
+        # for day 4
+        phoneUsage_day4 = pd.read_csv("review_entries_day4.csv")
+        phoneUsage_day4['startTime'] = pd.to_datetime(phoneUsage_day4['startTime']).dt.time
+        phoneUsage_day4['endTime'] = pd.to_datetime(phoneUsage_day4['endTime']).dt.time
+        phoneUsage_day4['timestamp'] = pd.to_datetime(phoneUsage_day4['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day4['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day4 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day4.iterrows():
+            for index_p, row_p in phoneUsage_day4.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day4["Ratings"] = final_rating
+
+        # for day 5
+        phoneUsage_day5 = pd.read_csv("review_entries_day5.csv")
+        phoneUsage_day5['startTime'] = pd.to_datetime(phoneUsage_day5['startTime']).dt.time
+        phoneUsage_day5['endTime'] = pd.to_datetime(phoneUsage_day5['endTime']).dt.time
+        phoneUsage_day5['timestamp'] = pd.to_datetime(phoneUsage_day5['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day5['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day5 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day5.iterrows():
+            for index_p, row_p in phoneUsage_day5.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day5["Ratings"] = final_rating
+
+        # for day 6
+        phoneUsage_day6 = pd.read_csv("review_entries_day6.csv")
+        phoneUsage_day6['startTime'] = pd.to_datetime(phoneUsage_day6['startTime']).dt.time
+        phoneUsage_day6['endTime'] = pd.to_datetime(phoneUsage_day6['endTime']).dt.time
+        phoneUsage_day6['timestamp'] = pd.to_datetime(phoneUsage_day6['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day6['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day6 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day6.iterrows():
+            for index_p, row_p in phoneUsage_day6.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day6["Ratings"] = final_rating
+
+        # for day 7
+        phoneUsage_day7 = pd.read_csv("review_entries_day7.csv")
+        phoneUsage_day7['startTime'] = pd.to_datetime(phoneUsage_day7['startTime']).dt.time
+        phoneUsage_day7['endTime'] = pd.to_datetime(phoneUsage_day7['endTime']).dt.time
+        phoneUsage_day7['timestamp'] = pd.to_datetime(phoneUsage_day7['timestamp'], unit='ms').dt.date
+        work=["Samsung Notes"]
+        leisure = ['전화', '메시지', 'Messenger', 'Instagram', 'YouTube', 'AfreecaTV', 'Facebook', '카카오톡']
+        ratings = []
+        final_rating = []
+        date = phoneUsage_day7['timestamp'].iloc[0]
+        date = date.strftime('%Y-%m-%d')
+        schedule_day7 = schedule[schedule['Date'] == date]
+        for index_s, row_s in schedule_day7.iterrows():
+            for index_p, row_p in phoneUsage_day7.iterrows():
+                if row_s["Start"] < row_p["endTime"] < row_s["End"]:
+                    if row_s["Category"] == "Sleep":
+                        rating = 0
+                    elif row_s["Category"] == "Work":
+                        if row_p["name"] in work:
+                            rating = 1
+                        elif row_p["name"] in leisure:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    elif row_s["Category"] == "Leisure":
+                        if row_p["name"] in leisure:
+                            rating = 1
+                        elif row_p["name"] in work:
+                            rating = 0.33
+                        else:
+                            rating = 0.66
+                    ratings.append(rating)
+            if len(ratings) > 0:
+                if statistics.mode(ratings) == 1:
+                    final_rating.append(1)
+                elif statistics.mode(ratings) == 0.66:
+                    final_rating.append(0.66)
+                elif statistics.mode(ratings) == 0.33:
+                    final_rating.append(0.33)
+                elif statistics.mode(ratings) == 0:
+                    final_rating.append(0)
+            else:
+                final_rating.append(1)
+            ratings = []
+
+        schedule_day7["Ratings"] = final_rating
+        combined_df = pd.concat([schedule_day1, schedule_day2, schedule_day3, schedule_day4, schedule_day5, schedule_day6, schedule_day7])
+        combined_df.reset_index(drop=True, inplace=True)
+        # heat map
+        heatmap_df = combined_df.pivot(index='Date', columns='Start', values='Ratings')
+        heatmap_df = heatmap_df.reset_index().melt(id_vars=['Date'], var_name='Start', value_name='Ratings')
+        def fill_ratings(row):
+            matching_rows = combined_df[(combined_df['Start'] <= row['Start']) & (combined_df['End'] >= row['Start'])]
+            if not matching_rows.empty:
+                return matching_rows['Ratings'].iloc[0]
+            else:
+                return row['Ratings']
+        heatmap_df['Ratings'] = heatmap_df.apply(lambda row: fill_ratings(row) if pd.isnull(row['Ratings']) else row['Ratings'], axis=1)
+        heatmap_df = heatmap_df.pivot(index='Date', columns='Start', values='Ratings')
         color_scale = [
             [0, "#D9D9D9"],
             [0.33, "#E16060"],
             [0.66, "#FBD167"],
             [1, "#65C089"],
         ]
-        # print(z)
-        for days in range(7):
-            for i in range(len(colors[days])):
-                for j in range(time[days][i]):
-                    z[days].append(colors[days][i])
-        # print(z)
-
         fig = px.imshow(
-            z,
+            heatmap_df,
             color_continuous_scale=color_scale,
-            labels=dict(x="Hour", y="Day of the week", color="Productivity"),
-            y=[
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-                "Sunday",
-            ],
+            labels=dict(x="Time", y="Date", color="Ratings"),
         )
         fig.layout.coloraxis.showscale = False
         fig.update_layout(height=400)
+        fig.update_layout(yaxis={'type': 'category'})
 
         # fig.update_layout(xaxis_range=[0,24])
         return dcc.Graph(id="graph", figure=fig)
